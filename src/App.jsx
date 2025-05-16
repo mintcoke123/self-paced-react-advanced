aae1ef9fc47a4fb8a75eeb8a2b596aad9e0d5d50
mintcoke123 <weetee1014@gmail.com>
2025-05-16 22:11:03 +0900

    self-paced-react-advanced 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-        Code It..
+      <h1>test</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
