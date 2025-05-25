3a37845079771fd7339a14379e5aab566a941475
mintcoke123 <weetee1014@gmail.com>
2025-05-25 22:02:36 +0900

    factor: db.json 정리, 사용하지 않는 context 파일 정리

--- src/pages/RestaurantListPage.jsx
+++ src/pages/RestaurantListPage.jsx
@@ -1,9 +1,9 @@
-import Header from "../components/Header/header";
+import Header from "../components/Header/Header";
 import MainContainer from "../components/Main/MainContainer";
 import AsideContainer from "../components/Asides/AsideContainer";
-import { DetailModalProvider } from '../context/DetailModalContext';
-import { AddModalProvider } from '../context/AddModalContext';
-import { RestaurantDataProvider } from '../context/RestaurantDataContext';
+import { DetailModalProvider } from "../context/DetailModalContext";
+import { AddModalProvider } from "../context/AddModalContext";
+import { RestaurantDataProvider } from "../context/RestaurantDataContext";
 
 function RestaurantListPage() {
   return (
